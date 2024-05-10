a495b2a0d201d02c68b19144f195e9009b4975ae
karthiksenniyappan <karthiksenniyappan76@gmail.com>
2024-05-10 18:20:20 +0530

    New version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ownotp',
-    version='0.0.2',
+    version='0.1.0',
     author='karthiksenniyappan',
     author_email='karthiksenniyappan76@gmail.com',
     description="Generate a time-based OTP using SHA-256 hashing algorithm",
--- tests/test_otp.py
+++ tests/test_otp.py
@@ -1,14 +1,22 @@
 import time
 import unittest
-from src.otp import generate_otp
+from ownotp.otp import generate_otp
 
 
 class TestOTP(unittest.TestCase):
     def test_generate_otp(self):
-        otp_one = generate_otp('9865321470')
+        otp_one = generate_otp('your_own_secret')
         time.sleep(15)
-        otp_two = generate_otp('9865321470')
+        otp_two = generate_otp('your_own_secret')
         self.assertEqual(otp_one, otp_two)
+
+    def test_generate_otp_digit(self):
+        otp = generate_otp('your_own_secret')
+        self.assertEqual(otp.isdigit(), True)
+
+    def test_generate_otp_not_digit(self):
+        otp = generate_otp('your_own_secret', only_digits=False, interval=10)
+        self.assertEqual(otp.isdigit(), False)
 
 
 if __name__ == '__main__':
